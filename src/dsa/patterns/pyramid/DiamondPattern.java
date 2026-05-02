00d0ba91328820372d03285949179fe7380078d2
ganeshcoder4043 <ganesha4043@gmail.com>
2026-05-03 01:57:36 +0530

    pyramid complete

---
+++ src/dsa/patterns/pyramid/DiamondPattern.java
@@ -0,0 +1,33 @@
+package dsa.patterns.pyramid;
+
+public class DiamondPattern {
+    public static void main(String[] args) {
+        int n = 5; // number of rows for the upper half
+
+        // upper half of the diamond
+        for (int i = 1; i <= n; i++) {
+            // print spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        // lower half of the diamond
+        for (int i = n - 1; i >= 1; i--) {
+            // print spaces
+            for (int j = n; j > i; j--) {
+                System.out.print(" ");
+            }
+            // print stars
+            for (int j = 1; j <= (2 * i - 1); j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ src/dsa/patterns/pyramid/PyramidPattern.java
@@ -0,0 +1,17 @@
+package dsa.patterns.pyramid;
+
+public class PyramidPattern {
+
+    public static void main(String[] args) {
+        System.out.println("this is PyramidPattern1");
+        for(int i=1;i<=5;i++) {
+            for(int s=5;s>=i;s--) {
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++) {
+                System.out.print("* ");  // must be space after star
+            }
+            System.out.println();
+        }
+    }
+}
